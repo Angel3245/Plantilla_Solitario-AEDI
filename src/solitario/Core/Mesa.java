6476783c9495ead71f04c67baac7c944041a81a1
Angel3245 <61286681+Angel3245@users.noreply.github.com>
2020-03-12 17:11:42 +0100

    Plantilla

---
+++ src/solitario/Core/Mesa.java
@@ -0,0 +1,21 @@
+/*
+* Representa la mesa de juego, donde estarán todas las cartas.
+* Tendrá dos partes diferenciadas la parte interior (donde inicialmente estarán colocadas las cartas correctamente para jugar al solitario).
+* Y los montones de descartes donde estarán colocadas las cartas por palo ordenadas de menor a mayor
+* Estructura: Se utilizarán TADs adecuados para su respresentación
+* Funcionalidad: colocar las cartas para iniciar el juego, quitar una carta de la parte interior, colocar una carta en el interior,
+* colocar una carta en el montón descarte correspondiente, visualizar cartas en la mesa, etc
+ */
+package solitario.Core;
+
+
+
+/**
+ *
+ * @author AEDI
+ */
+public class Mesa {
+    
+   
+    
+}
